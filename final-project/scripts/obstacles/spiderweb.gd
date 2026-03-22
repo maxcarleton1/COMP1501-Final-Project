cd25bd5979ca149609db987c3ce4f227346cd3ce
Siri Strassner <siri.strassner@gmail.com>
2026-03-22 19:05:09 -0400

    spiderweb finished

--- final-project/scripts/obstacles/spiderweb.gd
+++ final-project/scripts/obstacles/spiderweb.gd
@@ -1,19 +1,25 @@
 extends Area2D
 
-@export var Slow_Modify := 2  # A value of 2 half the player speed
+@export var Slow_Modify := 1.25  # A value of 2 half the player speed
 var slow_scalar :=0.5
+var players := []
 
 func _ready() -> void:
 	slow_scalar = 1/Slow_Modify
+
+func _physics_process(delta: float) -> void:
+	for p in players:
+		if p:
+			p.velocity *= slow_scalar
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		if not body.falling:
 			print("In slow zone.")
-			body.velocity = slow_scalar * body.velocity
+			players.append(body)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		if not body.falling:
 			print("Left slow zone.")
-			body.velocity = Slow_Modify * body.velocity
+			players.erase(body)

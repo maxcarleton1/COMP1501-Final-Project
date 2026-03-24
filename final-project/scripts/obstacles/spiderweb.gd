f816cd7acaf2d10a772babd17464b0535f2f8d0c
MaxSobota <maxsobota@cmail.carleton.ca>
2026-03-23 23:58:13 -0400

    Revert "Merge pull request #70 from maxcarleton1/obst3"
    This reverts commit 233043e11a43eb4a0a662cbf16cf2074c02f50b8, reversing
    changes made to a4ace1616854a0783842b26582d5431b95d5243a.

--- final-project/scripts/obstacles/spiderweb.gd
+++ final-project/scripts/obstacles/spiderweb.gd
@@ -1,12 +1,16 @@
 extends Area2D
 
-@export var Slow_Scalar :=0.5
+@export var Slow_Modify := 1.25  # A value of 2 half the player speed
+var slow_scalar :=0.5
 var players := []
+
+func _ready() -> void:
+	slow_scalar = 1/Slow_Modify
 
 func _physics_process(delta: float) -> void:
 	for p in players:
 		if p:
-			p.velocity *= Slow_Scalar
+			p.velocity *= slow_scalar
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
